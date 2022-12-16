0131d97f1c358e3f46fec06aa4274c4358cf5648
Knox-Phillips <Knox-Phillips@citypolyhs.org>
2022-12-16 22:34:32 +0000

    completed 5 out of 7 questions will refactor

--- code-wars-sotutions.js
+++ code-wars-sotutions.js
@@ -17,3 +17,59 @@ function solution(number){
   }
   return result
 }
+
+//2
+/*
+    Step 1: Make a conditional statement asking if the length is greater than 3 to return the first two names and then subtract names.length by 2 to get the rest of the people that like this
+    Step 2: Continue conditional by asking if the length is equal to three to make the string returning the first three names
+    Step 3: continue by asking if the conditional is equal to two and return those two names with the rest of the string
+    Step 4:Check if string is equal to one and return the appropriate string
+    Step 5: If none of the above is true return the last message
+*/
+function likes(names) {
+  return names.length > 3 ? `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+      :  names.length == 3 ? `${names[0]}, ${names[1]} and ${names[2]} like this`
+      :   names.length == 2 ? `${names[0]} and ${names[1]} like this`
+      :   names.length == 1 ?  `${names[0]} likes this`
+      : `no one likes this`
+}
+//3
+/*
+    Step 1:Initiate the higher oderer array method filter and provide a paramater
+    Step 2: Provide a predicate for the method to use while filtering the array that says you may only return a number whos first indexOf equals its lastIndexOf
+    Step 3: Use bracket notation to return the single element at index 0 because filter creates an array
+*/
+function findUniq(arr) {
+  return arr.filter(elem => arr.indexOf(elem) == arr.lastIndexOf(elem))[0]
+}
+//4
+/*
+    Step 1: Initiate a variable that holds all the numbers in the `integers` array that are even
+    Step 2: Initiate a variable that holds all the numbers in the 
+    `integers` array that are odd
+    Step 3: Compare which lengths are shorter and return the shortest array at the index of 0 since it will only have one value
+*/
+function findOutlier(integers){
+  let firstLength = integers.filter(elem => elem % 2 == 0)
+  let secondLength = integers.filter(elem => elem % 2 !== 0)
+  return firstLength.length < secondLength.length ? firstLength[0] : secondLength[0]
+}
+//5
+/*
+    Step 1: Create two empty arrays to hold the value of array pushed and to hold the zeroes
+    Step 2: Setup a for loop to go until the original arrays length
+    Step 3: Make a conditional saying if `arr[i] !== 0` than push that `arr[i]` into newarr, if else push it into the zeroes array
+    Step 4: use the .concat feature to join the arrays together at then end of newArr and return it
+*/
+function moveZeros(arr) {
+  let newArr = []
+  let zeroes = []
+  for(let i = 0;i<arr.length;i++) {
+    if(arr[i] !== 0) {
+      newArr.push(arr[i])
+    }else {
+      zeroes.push(arr[i])
+    }
+  }
+ return newArr.concat(zeroes)
+}
